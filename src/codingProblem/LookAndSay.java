a5090c97a53371e75c248c2989d7985109ec9489
Lorenzo Rossella <lorenzorossapp@gmail.com>
2021-03-05 15:44:59 +0100

    Corretto e aggiunta la possibilità di scegliere quante iterazioni eseguire

--- src/codingProblem/LookAndSay.java
+++ src/codingProblem/LookAndSay.java
@@ -7,39 +7,55 @@ public class LookAndSay {
     public static void main(String[] args) {
 
         String num = "";
+        int it = 0;
         Scanner in = new Scanner(System.in);
         System.out.println("Inserisci una sequenza di numeri: ");
-        num = in.nextLine();
-        System.out.println("ecco: "+LookAndSay.counter(num));
+        num = in.nextLine()+"-";
+        System.out.println("Quante iterazioni vuoi che vengano fatte? ");
+        it = in.nextInt();
+        LookAndSay.counter(num, it);
 
 
     }
 
-    public static String counter(String n){
+    public static void counter(String sequenza, int cicli){
 
         int n1 = 0;
         String soluzione = "";
         char tmp = '0';
+        int ite = 0;
+        while(cicli>0) {
+            for (int i = 0; i < sequenza.length(); i++) {
+                char carattere = sequenza.charAt(i);
+                tmp = carattere;
+                for (int j = i; j < sequenza.length(); ) {
 
-        for(int i = 0; i<n.length();i++){
-            char carattere = n.charAt(i);
-            tmp = carattere;
-            for(int j = i; j<n.length();){
-                if(tmp==n.charAt(j)){
-                    n1++;
-                    j++;
-                }else{
-                    soluzione = soluzione+""+n1 +""+ tmp;
-                    i += n1-1;
-                    n1 = 0;
-                    break;
+                        if (tmp == sequenza.charAt(j)) {
+                            if(tmp == '-'){
+                                break;
+                            }
+                            n1++;
+                            j++;
+                        } else {
+                            soluzione = soluzione + "" + n1 + "" + tmp;
+                            i += n1 - 1;
+                            n1 = 0;
+                            break;
+                        }
+
+
                 }
-
             }
+            ite++;
+            System.out.println("Iterazione numero: "+ite+" Soluzione: "+soluzione);
+            sequenza = soluzione+"-";
+            soluzione = "";
+            cicli--;
+            System.out.println("Cicli rimanenti: "+cicli);
         }
 
 
-        return soluzione;
+
     }
 
 }
